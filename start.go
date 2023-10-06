20e95563775a75477dc307d3804027e839517221
0xTopaz <myunghwan@imantisco.com>
2023-10-06 16:42:13 +0900

    feat : query entity

--- start.go
+++ start.go
@@ -6,6 +6,7 @@ import (
 	"log"
 
 	"entdemo/ent"
+	"entdemo/ent/user"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -32,5 +33,19 @@ func CreateUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
         return nil, fmt.Errorf("failed creating user: %w", err)
     }
     log.Println("user was created: ", u)
+    return u, nil
+}
+
+func QueryUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+    u, err := client.User.
+        Query().
+        Where(user.Name("a8m")).
+        // `Only` fails if no user found,
+        // or more than 1 user returned.
+        Only(ctx)
+    if err != nil {
+        return nil, fmt.Errorf("failed querying user: %w", err)
+    }
+    log.Println("user returned: ", u)
     return u, nil
 }
